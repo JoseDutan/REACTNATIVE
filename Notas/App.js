b44038fdc3b2169f73fbe906c4f35906a75236c6
JoseDutan <josedutan86@gmail.com>
2024-05-16 23:54:47 -0500

    RETO 30 COMPLETADO

--- Notas/App.js
+++ Notas/App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { GradeForm } from './app/screens/GradeForm';
+import { ListGrades } from './app/screens/ListGrades';
 export default function App() {
+  const StackGrades = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StackGrades.Navigator>
+      <StackGrades.Screen name='ListGradeNav' component={ListGrades} />
+        <StackGrades.Screen name='GradeFormNav' component={GradeForm} />
+      </StackGrades.Navigator>
+    </NavigationContainer>
   );
 }
 
